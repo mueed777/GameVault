df2b783100ef31a7696daf07cb42ab4a6624c37c
your <abdulmueedhassan0@gmail.com>
2024-11-30 10:32:25 -0800

    AH: handle error while fetching genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,34 +1,50 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertIcon,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Text,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import croppedImageUrl from "../services/image-url";
 import GenresSkeleton from "./GenresSkeleton";
 
 const GenreList = () => {
-  const { data, isLoading } = useGenres();
+  const { data, isLoading, error } = useGenres();
   const genreListSkeleton = 15;
 
   return (
-    // TODO: handle error case scenario here
-    <List>
-      {isLoading &&
-        Array.from({ length: genreListSkeleton }).map((_, index) => (
-          <GenresSkeleton key={index} />
-        ))}
-      {data.map((genre) => (
-        <HStack>
-          <ListItem key={genre.id} paddingY="5px">
+    <>
+      {error ? (
+        <Alert status="error">
+          <AlertIcon />
+          {error}
+        </Alert>
+      ) : (
+        <List>
+          {isLoading &&
+            Array.from({ length: genreListSkeleton }).map((_, index) => (
+              <GenresSkeleton key={index} />
+            ))}
+          {data.map((genre) => (
             <HStack>
-              <Image
-                src={croppedImageUrl(genre.image_background)}
-                boxSize="32px"
-                borderRadius={8}
-              ></Image>
-              <Text fontSize="lg">{genre.name}</Text>
+              <ListItem key={genre.id} paddingY="5px">
+                <HStack>
+                  <Image
+                    src={croppedImageUrl(genre.image_background)}
+                    boxSize="32px"
+                    borderRadius={8}
+                  ></Image>
+                  <Text fontSize="lg">{genre.name}</Text>
+                </HStack>
+              </ListItem>
             </HStack>
-          </ListItem>
-        </HStack>
-      ))}
-    </List>
+          ))}
+        </List>
+      )}
+    </>
   );
 };
 
