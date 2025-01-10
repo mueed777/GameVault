8246e29740c8c83d24b7b847cd6212f6eaeb2879
your <abdulmueedhassan0@gmail.com>
2025-01-09 16:51:18 -0800

    AH: remove padding from darklightmode

--- src/components/DarkLightMode.tsx
+++ src/components/DarkLightMode.tsx
@@ -4,16 +4,14 @@ const DarkLightMode = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <HStack paddingRight={"15px"}>
-      <Switch
-        isChecked={colorMode === "dark"}
-        onChange={toggleColorMode}
-        colorScheme={"green"}
-        whiteSpace="nowrap"
-      >
-        {colorMode === "light" ? "Dark" : "Light"}
-      </Switch>
-    </HStack>
+    <Switch
+      isChecked={colorMode === "dark"}
+      onChange={toggleColorMode}
+      colorScheme={"green"}
+      whiteSpace="nowrap"
+    >
+      {colorMode === "light" ? "Dark" : "Light"}
+    </Switch>
   );
 };
 
